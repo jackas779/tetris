042a682bbb5c6420a0da238529d2c5262ff55703
Nicolas Ardila <jackas779@gmail.com>
2025-04-14 21:14:17 -0500

    se crean y actualizan funciones

--- game.js
+++ game.js
@@ -6,6 +6,14 @@ const WIDTH_BOARD = 300
 const HEIGHT_BOARD = 500
 const COLUMSN_BOARD = WIDTH_BOARD / BLOCK_SIZE
 const ROWS_BOARD = HEIGHT_BOARD / BLOCK_SIZE
+const FILL = 0.8
+const board = Array(ROWS_BOARD).fill(0).map(e => {
+  return Array(COLUMSN_BOARD).fill(0)
+})
+
+let y = 2
+const x = 5
+// console.log(board)
 
 canvas.width = WIDTH_BOARD
 canvas.height = HEIGHT_BOARD
@@ -18,6 +26,18 @@ function draw () {
   context.fillRect(0, 0, canvas.width, canvas.height)
 
   drawGrid(COLUMSN_BOARD, ROWS_BOARD)
+
+  context.fillStyle = 'purple'
+
+  const piece = [
+    [0, 1, 0],
+    [1, 1, 1]
+  ]
+
+  drawPiece(piece, x, y)
+  y = gravity(y)
+  console.log(y)
+  // window.requestAnimationFrame(draw)
 }
 
 function drawGrid (width, heigth) {
@@ -29,7 +49,7 @@ function drawGrid (width, heigth) {
 }
 
 function drawBlockMatriz (x, y) {
-  context.strokeStyle = 'black'
+  context.strokeStyle = 'gray'
   context.lineWidth = 0.1
   context.strokeRect(x, y, 1, 1)
 }
@@ -39,4 +59,32 @@ function drawBlockVisualization (x, y) {
   context.lineWidth = 0.1
   context.strokeRect(y + 0.1, x + 0.1, 0.8, 0.8)
 }
+
+function drawPiece (piece, x, y) {
+  const ejeX = x
+  piece.forEach(array => {
+    array.forEach(e => {
+      if (e === 1) {
+        context.fillRect(x + 0.1, y + 0.1, FILL, FILL)
+      }
+      x++
+    })
+    x = ejeX
+    y++
+  })
+}
+
+function gravity (y) {
+  if (y < ROWS_BOARD) {
+    y++
+  } else {
+    y = 2
+  }
+  setTimeout(() => {
+    console.log(y)
+  }, 10000)
+
+  return y
+}
+
 draw()
